a66c3de978a40c562c5614d80a92ccae01032f65
Rominer-11 <Augustcho2000@gmail.com>
2026-06-20 09:18:35 -0700

    Patch 2.0.1
    Fixed enchantment tables so that they no longer lie to the player.

--- SMPEight/src/me/rominer_11/EnchantControl.java
+++ SMPEight/src/me/rominer_11/EnchantControl.java
@@ -1,25 +1,57 @@
 package me.rominer_11;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentOffer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
 
 public class EnchantControl implements Listener 
 {
+	private final Map<UUID, EnchantmentOffer[]> playerEnchantPreserver = new HashMap<>();
+	
 	@EventHandler
 	public void onEnchantTableOpen(PrepareItemEnchantEvent event)
 	{
 		EnchantmentOffer[] offers = event.getOffers();
+		playerEnchantPreserver.put(event.getEnchanter().getUniqueId(), offers);
 		for (EnchantmentOffer offer : offers)
 		{
 			if (offer != null)
 			{
 				offer.setCost((int) (offer.getCost() * 1.5));
+			}
+		}
+	}
+	@EventHandler
+	public void onEnchant(EnchantItemEvent event)
+	{
+		// Add the intended enchant displayed at offer screen
+		Player player = event.getEnchanter();		
+		ItemStack item = event.getItem();
+		int button = event.whichButton();
+		EnchantmentOffer[] offers = playerEnchantPreserver.remove(player.getUniqueId());
+		EnchantmentOffer offer = offers[button];
+		item.addEnchantment(offer.getEnchantment(), offer.getEnchantmentLevel());
+		
+		// Remove conflicting enchants
+		Map<Enchantment, Integer> otherEnchants = event.getEnchantsToAdd();
+		for (Map.Entry<Enchantment, Integer> enchantLevelPair : otherEnchants.entrySet())
+		{
+			if (enchantLevelPair.getKey().conflictsWith(offer.getEnchantment()) || enchantLevelPair.getKey().equals(offer.getEnchantment()))
+			{
+				otherEnchants.remove(enchantLevelPair.getKey());
 			}
 		}
 	}
