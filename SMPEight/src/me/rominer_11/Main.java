1a39463ea1e3b786f644aad99f6c7e6475b21ea3
GhoststarYT <ghasttrooper@gmail.com>
2026-06-19 20:53:19 -0500

    Add+InfoBook+guide+and+smpguide+command

--- SMPEight/src/me/rominer_11/Main.java
+++ SMPEight/src/me/rominer_11/Main.java
@@ -1,8 +1,11 @@
 package me.rominer_11;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin
+public class Main extends JavaPlugin implements org.bukkit.command.CommandExecutor
 {
 	@Override
 	public void onEnable()
@@ -16,9 +19,28 @@ public class Main extends JavaPlugin
 		getServer().getPluginManager().registerEvents(new BlockControl(), this);
 		getServer().getPluginManager().registerEvents(new EnchantControl(), this);
 		getServer().getPluginManager().registerEvents(new BedControl(), this);
+		getServer().getPluginManager().registerEvents(new InfoBook(), this);
+		getCommand("smpguide").setExecutor(this);
 		System.out.println("SMPEight enabled!");
 		System.out.println("SMPEight is best experienced on HARD difficulty, with keepInventory FALSE, and with playersSleepingPercentage 50");
 	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	{
+		if (command.getName().equalsIgnoreCase("smpguide"))
+		{
+			if (sender instanceof Player player)
+			{
+				player.openBook(InfoBook.createBook());
+				return true;
+			}
+			sender.sendMessage("Only players can use this command.");
+			return true;
+		}
+		return false;
+	}
+
 	@Override
 	public void onDisable()
 	{
